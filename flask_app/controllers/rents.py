030ba15cf1e59da9f805df1bb636f932333bf35b
Kyle Deato <kyledeato@gmail.com>
2022-03-28 19:36:40 -0700

    add create a post

--- flask_app/controllers/rents.py
+++ flask_app/controllers/rents.py
@@ -1,8 +1,10 @@
 from flask import render_template, session, redirect, request, flash
 from flask_app import app
-# from flask_app.models.user import User
+from flask_app.models.rent import Rent
+from flask_app.models.user import User
 
-# DASHBOARD
+#DISPLAY ROUTES -----------------------
+# dashboard
 @app.route('/')
 def index():
     return redirect('/rentboard')
@@ -16,5 +18,22 @@ def dashboard():
 
 @app.route('/rentboard')
 def rentboard():
-    return render_template('rentboard.html')
+    rents = Rent.get_all()
+    # logged_in_user = User.get_by_email({"id": session['user_id']})
+    return render_template('rentboard.html', rents = rents)
 
+@app.route('/create')
+def create():
+    if 'user_id' not in session:
+        return redirect('/login')
+    return render_template('create.html')
+
+#ACTION ROUTES -----------------------
+@app.route('/create-post', methods=['POST'])
+def create_post():
+    if 'user_id' not in session:
+        return redirect('/login')
+    if not Rent.is_valid(request.form):
+        return redirect ('/create')
+    Rent.create(request.form)
+    return redirect ('/rentboard')
---
+++ flask_app/models/rent.py
@@ -0,0 +1,52 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app import app
+from flask_app.models import user
+
+class Rent: 
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.description = data['description']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    
+    @classmethod
+    def get_all(cls):
+        query = 'SELECT * FROM rents JOIN users ON rents.user_id = users.id;'
+        results = connectToMySQL('rentr_db').query_db(query)
+        rents = []
+        if results:
+            for row in results:
+                temp_rents = cls(row)
+                user_data = {
+                    "id": row["users.id"],
+                    "first_name": row["first_name"],
+                    "last_name": row["last_name"],
+                    "email": row["email"],
+                    "password": row["password"],
+                    "created_at": row["users.created_at"],
+                    "updated_at": row["users.updated_at"]
+                }
+                temp_rents.creator = user.User(user_data)
+                rents.append(temp_rents)
+            return rents
+
+    @classmethod
+    def create(cls, data):
+        query = 'INSERT INTO rents (name, description, location, user_id) VALUES (%(name)s, %(description)s, %(location)s, %(user_id)s)'
+        return connectToMySQL('rentr_db').query_db(query, data)
+    
+    @staticmethod
+    def is_valid(data):
+        is_valid = True
+        if len(data['name']) < 2:
+            flash("Name must be at least 2 characters long.")
+            is_valid = False
+        if len(data['description']) < 10:
+            flash("Description should be at least 10 characters long.")
+            is_valid = False
+        if len(data['location']) < 5:
+            flash("Location should be at least 5 characters long.")
+            is_valid = False
+        return is_valid
